a48361bb516c33286e493e06a23f744d3689d59a
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-06-30 12:28:40 +0300

    add refreshUser.rejected to slice

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -38,6 +38,9 @@ const authSlice = createSlice({
         state.user = action.payload;
         state.isLoggedIn = true;
         state.isRefreshing = false;
+      })
+      .addCase(refreshUser.rejected, state => {
+        state.token = null;
       }),
 });
 
