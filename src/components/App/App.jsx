58ed6e376b3f98dec5cc41ac6f0d5403f786f10e
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-06-30 23:09:13 +0300

    add lazy and suspense

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,6 @@
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
-import HomePage from '../../pages/HomePage/HomePage';
-import RegistrationPage from '../../pages/RegistrationPage/RegistrationPage';
-import LoginPage from '../../pages/LoginPage/LoginPage';
-import ContactsPage from '../../pages/ContactsPage/ContactsPage';
-import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
 import Layout from '../Layout/Layout';
 import AppBar from '../AppBar/AppBar';
 import { useEffect } from 'react';
@@ -14,6 +9,19 @@ import { selectIsRefreshing } from '../../redux/auth/selectors';
 import Loader from '../Loader/Loader';
 import RestrictedRoute from '../RestrictedRoute';
 import PrivateRoute from '../PrivateRoute';
+import { Suspense, lazy } from 'react';
+
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
+const RegistrationPage = lazy(() =>
+  import('../../pages/RegistrationPage/RegistrationPage')
+);
+const LoginPage = lazy(() => import('../../pages/LoginPage/LoginPage'));
+const ContactsPage = lazy(() =>
+  import('../../pages/ContactsPage/ContactsPage')
+);
+const NotFoundPage = lazy(() =>
+  import('../../pages/NotFoundPage/NotFoundPage')
+);
 
 function App() {
   const dispatch = useDispatch();
@@ -29,26 +37,27 @@ function App() {
   ) : (
     <Layout>
       <AppBar />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
+          <Route
+            path="/register"
+            element={<RestrictedRoute component={<RegistrationPage />} />}
+          />
+          <Route
+            path="/login"
+            element={<RestrictedRoute component={<LoginPage />} />}
+          />
 
-        <Route
-          path="/register"
-          element={<RestrictedRoute component={<RegistrationPage />} />}
-        />
-        <Route
-          path="/login"
-          element={<RestrictedRoute component={<LoginPage />} />}
-        />
+          <Route
+            path="/contacts"
+            element={<PrivateRoute component={<ContactsPage />} />}
+          />
 
-        <Route
-          path="/contacts"
-          element={<PrivateRoute component={<ContactsPage />} />}
-        />
-
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
